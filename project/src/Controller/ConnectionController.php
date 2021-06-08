e715e92687c02e2bd3227a3ade5775fd5fd9a331
DEADL0CK <Hydrark07@gmail.com>
2021-06-08 18:31:00 +0200

    Ajout des nouvelles pages et leur contenu

--- project/src/Controller/ConnectionController.php
+++ project/src/Controller/ConnectionController.php
@@ -12,12 +12,30 @@ use function PHPSTORM_META\type;
 class ConnectionController extends AbstractController
 {
 
-    function userConnected(){
-        $login = isset($_COOKIE['login'])?$_COOKIE['login']:null;
-        $token = isset($_COOKIE['token'])?$_COOKIE['token']:null;
+    function userConnected()
+    {
+        $login = isset($_COOKIE['login']) ? $_COOKIE['login'] : null;
+        $token = isset($_COOKIE['token']) ? $_COOKIE['token'] : null;
 
-        if($login && $token){
+        if ($login && $token) {
             return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+    * @Route("/disconnect", name="User deconnection system")
+    */
+    function userDisconnected()
+    {
+        $login = isset($_COOKIE['login']) ? $_COOKIE['login'] : null;
+        $token = isset($_COOKIE['token']) ? $_COOKIE['token'] : null;
+
+        if (($login && $token) || $login || $token) {
+            setcookie('login', null, time() - 365243600, '/', null, false, false);
+            setcookie('token', null, time() - 365243600, '/', null, false, false);
+            return $this->redirectToRoute('User connection page');
         } else {
             return false;
         }
@@ -26,14 +44,24 @@ class ConnectionController extends AbstractController
     /**
      * @Route("/co", name="User connection page")
      */
-    public function page_connection(){
+    public function page_connection()
+    {
         return $this->render('connexion/UserConnectionPage.html.twig', array());
+    }
+
+    /**
+     * @Route("/inscription", name="Inscription page")
+     */
+    public function page_inscription()
+    {
+        return $this->render('connexion/UserRegister.html.twig', array());
     }
 
     /**
      * @Route("/log_in", name="User connection function")
      */
-    public function user_connection() {
+    public function user_connection()
+    {
         $login = $_POST['login'];
         $password = $_POST['password'];
 
@@ -43,23 +71,23 @@ class ConnectionController extends AbstractController
             'salt' => 10
         ];
 
-        $response = $client->request('POST', 'http://'.$_ENV['IPV4'].':3000/users/login', [
+        $response = $client->request('POST', 'http://' . $_ENV['IPV4'] . ':3000/users/login', [
             'body' => [
                 'login' => $login,
                 'password' => $password
             ]
         ]);
 
-        $content = json_decode($response->getContent(),true);
 
-        if($response->getStatusCode() === 200){
+        if ($response->getStatusCode() === 200) {
             //Execution quand un utilisateur a réussi à se connecter
+            $content = json_decode($response->getContent(), true);
             setcookie('login', $content["userLogin"], time() + 365243600, '/', null, false, false);
             setcookie('token', $content["userToken"], time() + 365243600, '/', null, false, false);
             return $this->redirectToRoute("Accueil");
         } else {
             //Execution quand la connexion a échoué
-            echo("Loupé");
+            echo ("Loupé");
             return $this->render('connexion/UserConnectionPage.html.twig', array());
         }
     }
