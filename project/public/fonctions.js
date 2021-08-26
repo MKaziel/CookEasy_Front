3753e339ae73de146ce8c7c2bfe4ae7c90b78f67
MKaziel <mkaziel@hotmail.com>
2021-08-26 15:59:50 +0200

    Update page de recherche

--- project/public/fonctions.js
+++ project/public/fonctions.js
@@ -6,3 +6,17 @@ function getCookie(name) {
     })
     return cookie[name];
 }
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
